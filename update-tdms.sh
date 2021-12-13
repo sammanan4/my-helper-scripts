ce535a0dde4f1fad51cb7f2e63a5596205c0a9f3
Sheikh Abdul Manan <sammanan4@gmail.com>
2021-12-13 18:38:32 +0530

    Create update-tdms.sh

---
+++ update-tdms.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Stopping airflow scheduler..."
+sudo service airflow-scheduler stop
+echo "Stopping airflow webserver..."
+sudo service airflow-webserver stop
+
+cd /home/sammanan4/office/tdms/CloudTDMS_V2/
+git pull
+
+
+echo "Updating code files..."
+sudo find /opt/cloudtdms/dags/ ! \( -name "bundle_dags" -o -name "app_dags" -o -name "dags" \) -exec rm -rf {} 2> /dev/null \;
+
+sudo cp /home/sammanan4/office/tdms/CloudTDMS_V2/CloudTDMS/dags/* -r /opt/cloudtdms/dags/
+
+echo "Updating libraries..."
+sudo rm -rf /opt/cloudtdms/libraries/*
+
+sudo cp /home/sammanan4/office/tdms/CloudTDMS_V2/CloudTDMS/libraries/* -r /opt/cloudtdms/libraries/
+
+
+echo "Updating permissions"
+sudo chown cloudtdms:cloudtdms -R /opt
+
+echo "Starting airflow webserver and airflow scheduler..."
+sudo service airflow-scheduler start
+sudo service airflow-webserver start
